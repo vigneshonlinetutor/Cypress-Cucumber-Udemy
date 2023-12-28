0284981e0daa049ad118f330dc6a7349282af73f
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-12-28 02:29:02 +0100

    Test Data with Fixture

--- cypress/e2e/cucumber/stepDefinitions/conduitLogin/conduitLogin_stepdef.js
+++ cypress/e2e/cucumber/stepDefinitions/conduitLogin/conduitLogin_stepdef.js
@@ -1,13 +1,17 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+beforeEach(function(){
+    cy.fixture('conduitLoginData').as('data')
+})
+
 Given('User is on the login page', function () {
     cy.visit('https://react-redux.realworld.io/')
     cy.get('a[href="#login"]').click()
 })
 
 When('User login with valid credentials', function () {
-    cy.get('input[placeholder="Email"]').type('cypressdemo@gmail.com')
-    cy.get('input[placeholder="Password"]').type('cypressdemo')
+    cy.get('input[placeholder="Email"]').type(this.data.validEmail)
+    cy.get('input[placeholder="Password"]').type(this.data.validPassword)
     cy.get('button[type="submit"]').click()
 })
 
